277014af5884f3d4a29dabeda20c2444e3178b66
Master Kenth <MasterKenth@gmail.com>
2021-06-07 18:20:50 +0200

    feat: add top-level error handling

--- pokemonDataScraper.ts
+++ pokemonDataScraper.ts
@@ -3,27 +3,36 @@ dotenv.config();
 
 import Scrape from "./src/scrape";
 import ScrapePaths from "./src/paths";
+import { exit } from "process";
 
 (async () => {
-  console.log("Running full Pokémon data scrape...");
+  try {
+    console.log("Running full Pokémon data scrape...");
 
-  await ScrapePaths.ensurePaths();
+    await ScrapePaths.ensurePaths();
 
-  const basicPokemonList = await Scrape.scrapePokemonIndexList();
-  const basicMovesList = await Scrape.scrapeMovesList();
+    const basicPokemonList = await Scrape.scrapePokemonIndexList();
+    const basicMovesList = await Scrape.scrapeMovesList();
 
-  const unscraped = await Scrape.getUnscraped(basicPokemonList, basicMovesList);
-  console.log(
-    `Unscraped Pokémon=${unscraped.pokemon.length} Moves=${unscraped.moves.length}`
-  );
+    const unscraped = await Scrape.getUnscraped(
+      basicPokemonList,
+      basicMovesList
+    );
+    console.log(
+      `Unscraped Pokémon=${unscraped.pokemon.length} Moves=${unscraped.moves.length}`
+    );
 
-  await Promise.all([
-    Scrape.slowScrapeAllPokemon(unscraped.pokemon),
-    Scrape.slowScrapeAllMoves(unscraped.moves),
-    Scrape.scrapeNaturesList(),
-    Scrape.scrapeAbilitiesList(),
-    Scrape.scrapeZMovesList(),
-  ]);
+    await Promise.all([
+      Scrape.slowScrapeAllPokemon(unscraped.pokemon),
+      Scrape.slowScrapeAllMoves(unscraped.moves),
+      Scrape.scrapeNaturesList(),
+      Scrape.scrapeAbilitiesList(),
+      Scrape.scrapeZMovesList(),
+    ]);
 
-  console.log("done!");
+    console.log("done!");
+  } catch (error) {
+    console.error("Error during scrape", error);
+    exit(-1);
+  }
 })();
